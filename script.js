c9272505e17f7d297bef4979fcd7bc5d8fc93bfa
Tudor Stroescu <tudorstroescu@yahoo.com>
2022-05-16 19:02:34 +0300

    reversed-output

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 const txtInput = document.querySelector('.inputs input');
 const checkBtn = document.querySelector('.inputs button');
+let filterInput;
+
+checkBtn.addEventListener("click", ()=>{
+    let reverseInput = filterInput.split('').reverse().join('');
+    //split('') - splits the input into characters, now we have an array of characters
+    //reverse - we reversed the array
+    //join('') - now we want the array elements without being separated with a comma
+    //so we have: 12345 -> arr[5]={1,2,3,4,5} -> arr[5]={5,4,3,2,1} -> arr.join('') -> 54321
+    console.log(reverseInput);
+})
 
 txtInput.addEventListener("keyup",()=>{
     //remove spaces and special characters
-    let filterInput = txtInput.value.replace(/[^A-Z0-9]/ig, '');
+    filterInput = txtInput.value.replace(/[^A-Z0-9]/ig, '');
     if(filterInput){
         return checkBtn.classList.add('active');
     }
