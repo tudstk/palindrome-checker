3e3753324a9dea16ded248d16dd250b1e72b9211
Tudor Stroescu <tudorstroescu@yahoo.com>
2022-05-16 19:10:03 +0300

    ignore-case-sensitive & output-palindrome

--- script.js
+++ script.js
@@ -8,12 +8,15 @@ checkBtn.addEventListener("click", ()=>{
     //reverse - we reversed the array
     //join('') - now we want the array elements without being separated with a comma
     //so we have: 12345 -> arr[5]={1,2,3,4,5} -> arr[5]={5,4,3,2,1} -> arr.join('') -> 54321
-    console.log(reverseInput);
+    if(filterInput != reverseInput){
+        return console.log("Not palindrome!");
+    }
+    console.log("Palindrome!");
 })
 
 txtInput.addEventListener("keyup",()=>{
     //remove spaces and special characters
-    filterInput = txtInput.value.replace(/[^A-Z0-9]/ig, '');
+    filterInput = txtInput.value.toLowerCase().replace(/[^A-Z0-9]/ig, '');
     if(filterInput){
         return checkBtn.classList.add('active');
     }
